b2e2af0e369695db51cdd9f0ed5532d18fbb4f17
arcanstone <allan.henriques@mail.utoronto.ca>
2025-08-04 21:02:00 -0400

    Fix batch script path quoting issue - v2.2.3
    - Fixed 'The system cannot find the file C:\Program' error
    - Proper quoting for paths with spaces (Program Files)
    - Simplified Chrome detection logic
    - Removed delayed expansion (not needed)
    - Now works correctly on all Windows systems

--- dist/install-extension.bat
+++ dist/install-extension.bat
@@ -1,5 +1,4 @@
 @echo off
-setlocal enabledelayedexpansion
 
 echo.
 echo ================================================
@@ -19,15 +18,17 @@ echo [1/3] Checking Chrome installation...
 
 REM Find Chrome executable
 set "CHROME_PATH="
-for %%i in (
-    "%ProgramFiles%\Google\Chrome\Application\chrome.exe"
-    "%ProgramFiles(x86)%\Google\Chrome\Application\chrome.exe"
-    "%LocalAppData%\Google\Chrome\Application\chrome.exe"
-) do (
-    if exist "%%i" (
-        set "CHROME_PATH=%%i"
-        goto :chrome_found
-    )
+if exist "%ProgramFiles%\Google\Chrome\Application\chrome.exe" (
+    set "CHROME_PATH=%ProgramFiles%\Google\Chrome\Application\chrome.exe"
+    goto :chrome_found
+)
+if exist "%ProgramFiles(x86)%\Google\Chrome\Application\chrome.exe" (
+    set "CHROME_PATH=%ProgramFiles(x86)%\Google\Chrome\Application\chrome.exe"
+    goto :chrome_found
+)
+if exist "%LocalAppData%\Google\Chrome\Application\chrome.exe" (
+    set "CHROME_PATH=%LocalAppData%\Google\Chrome\Application\chrome.exe"
+    goto :chrome_found
 )
 
 echo Chrome not found in standard locations.
@@ -36,7 +37,7 @@ pause
 exit /b 1
 
 :chrome_found
-echo Chrome found: !CHROME_PATH!
+echo Chrome found: %CHROME_PATH%
 
 echo.
 echo [2/3] Opening Chrome with extension loading page...
@@ -45,8 +46,8 @@ echo.
 REM Get current directory
 set "EXT_DIR=%CD%"
 
-REM Open Chrome with extensions page and developer mode
-start "" "!CHROME_PATH!" --new-window --args --enable-extensions --load-extension="!EXT_DIR!" chrome://extensions/
+REM Open Chrome with extensions page
+start "" "%CHROME_PATH%" --new-window chrome://extensions/
 
 echo [3/3] Extension loading initiated!
 echo.
@@ -55,7 +56,7 @@ echo 1. Chrome should open with the Extensions page
 echo 2. If "Developer mode" is OFF, toggle it ON (top-right)
 echo 3. The extension should load automatically
 echo 4. If not, click "Load unpacked" and select this folder:
-echo    %EXT_DIR%
+echo    "%EXT_DIR%"
 echo.
 echo Press any key to close this window...
 pause >nul
